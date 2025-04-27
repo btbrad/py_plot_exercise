a5cd880920106efabbb62f04462e25bccd2cbc30
btbrad <biantaobrad@gmail.com>
2025-04-27 21:55:06 +0800

    调整尺寸以适应屏幕

--- rw_visual.py
+++ rw_visual.py
@@ -5,15 +5,15 @@ from random_walk import RandomWalk
 while True:
 
     # 创建一个RandomWalk实例
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # 将所有的点都绘制出来
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
-            edgecolors='none', s=15)
+            edgecolors='none', s=1)
     ax.set_aspect('equal')
 
     # 突出起点和终点
@@ -24,7 +24,7 @@ while True:
     # 隐藏坐标轴
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-    
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
