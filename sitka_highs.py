3323f485ac09e70dd7f0e9f0b39dc7748695c4f9
btbrad <biantaobrad@gmail.com>
2025-04-28 22:08:13 +0800

    在绘制一个数据序列

--- sitka_highs.py
+++ sitka_highs.py
@@ -11,20 +11,23 @@ reader = csv.reader(lines)
 header_row = next(reader)
 
 # 提取日期和最高温度
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 # 根据最高温度绘图
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
 
 # 设置绘图的格式
-ax.set_title("Daily high temperatures, 2021", fontsize=24)
+ax.set_title("Daily High and Low temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
