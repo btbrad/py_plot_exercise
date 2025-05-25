65484c0daa5f8873fbc408739cc7246793df64d8
btbrad <biantaobrad@gmail.com>
2025-05-25 20:33:07 +0800

    修改样式

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -15,14 +15,23 @@ print(f"Complete results: {not response_dict['incomplete_results'] }")
 
 # 处理有关仓库的信息
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{ repo_url }'>{repo_name}</a>"
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
+
+    # 创建悬停文本
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_texts.append(f"{owner}<br>{description}")
 
 # 可视化
 title  = 'Most-Starred Python Projects on GitHub'
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts)
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)    
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
 fig.show()
